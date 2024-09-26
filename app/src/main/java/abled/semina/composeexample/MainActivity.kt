f369451ded3613edb51e3b3bb636d038bbf8dc0e
harry <gudtjs9556@naver.com>
2024-09-26 16:23:06 +0900

    컴포즈 열과 행 만들기

--- app/src/main/java/abled/semina/composeexample/MainActivity.kt
+++ app/src/main/java/abled/semina/composeexample/MainActivity.kt
@@ -11,7 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import abled.semina.composeexample.ui.theme.ComposeExampleTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 
@@ -31,26 +35,41 @@ class MainActivity : ComponentActivity() {
 // 함수가 내부에서 다른 @Composable을 호출할 수 있다.
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-Surface(color = MaterialTheme.colorScheme.primary){
-    Text(
-        text = "Hello $name!",
-        modifier = modifier.padding(24.dp)
-    )
+Surface(color = MaterialTheme.colorScheme.primary,
+modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)){
+    Row(modifier = Modifier.padding(24.dp)){
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Hello")
+            Text(text = name)
+        }
+        ElevatedButton(
+            onClick = { }
+        ){
+            Text("Show more")
+        }
+    }
+
 }
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
+fun MyApp(modifier: Modifier = Modifier,
+names: List<String> = listOf("World", "Compose")
+){
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ){
-        Greeting("Hello")
+        Column(modifier = modifier.padding(vertical = 4.dp)) {
+            for(name in names){
+                Greeting(name = name)
+            }
+        }
     }
 }
 
 //미리보기
-@Preview(showBackground = true, name = "Text preview")
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     ComposeExampleTheme {
