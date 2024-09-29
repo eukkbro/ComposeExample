a9ec10dc7e5dd46886b864aedfe070aa59271d2f
harry <gudtjs9556@naver.com>
2024-09-29 21:35:52 +0900

    상태 호이스팅연습

--- app/src/main/java/abled/semina/composeexample/MainActivity.kt
+++ app/src/main/java/abled/semina/composeexample/MainActivity.kt
@@ -11,13 +11,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import abled.semina.composeexample.ui.theme.ComposeExampleTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 import kotlin.math.exp
@@ -27,67 +32,109 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeExampleTheme {
-                // A surface container using the 'background' color from the theme
-               MyApp(modifier = Modifier.fillMaxSize())
+                MyApp(modifier =  Modifier.fillMaxSize())
             }
         }
     }
 }
 
-// 구성 가능한 함수
-// 함수가 내부에서 다른 @Composable을 호출할 수 있다.
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier){
+
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if(shouldShowOnBoarding){
+            OnboardingScreen(onContinueClicked = {shouldShowOnBoarding = false})
+        }
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier){
+
+    Column(modifier = modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp ),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+
+    }
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    val expanded = remember{mutableStateOf(false)}
-
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
-
-Surface(color = MaterialTheme.colorScheme.primary,
-modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)){
-
-    Row(modifier = Modifier.padding(24.dp)){
-
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
-        ) {
-
-            Text(text = "Hello")
-            Text(text = name)
-        }
-        ElevatedButton(
-            onClick = { expanded.value = !expanded.value }
-        ){
-            Text(if(expanded.value) "Show less" else "Show more")
-        }
-    }
-
-}
-}
-
-@Composable
-fun MyApp(modifier: Modifier = Modifier,
-names: List<String> = listOf("World", "Compose")
-){
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ){
-        Column(modifier = modifier.padding(vertical = 4.dp)) {
-            for(name in names){
-                Greeting(name = name)
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
 }
+
+
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+){
+    Column(modifier = modifier.padding(vertical = 4.dp)){
+        for( name in names){
+            Greeting(name = name)
+        }
+    }
+}
+
 
 //미리보기
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     ComposeExampleTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposeExampleTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeExampleTheme {
+        OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
     }
 }
