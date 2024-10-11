22ea56d9169627b20315e05d723fe0ac9f987593
harry <gudtjs9556@naver.com>
2024-10-11 13:06:24 +0900

    홈화면 구성

--- app/src/main/java/abled/semina/composeexample/MainActivity.kt
+++ app/src/main/java/abled/semina/composeexample/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package abled.semina.composeexample
 
 import android.os.Bundle
@@ -43,6 +45,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
@@ -67,7 +70,6 @@ class MainActivity : ComponentActivity() {
 
 
 // 검색창
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier
@@ -353,7 +355,25 @@ fun SootheBottomNavigationPreview(){
     }
 }
 
+@Composable
+fun MySootheAppPortrait() {
+    ComposeExampleTheme {
+        Scaffold(
+            bottomBar = { SootheBottomNavigation() }
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
 
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun MySootheAppPortraitPreview(){
+    ComposeExampleTheme {
+        MySootheAppPortrait()
+    }
+}
 
 
 
