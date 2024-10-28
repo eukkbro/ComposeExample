d373b604f78942d9eebb0dea652788ae5f9d5728
harry <gudtjs9556@naver.com>
2024-10-28 22:59:50 +0900

    뷰모델 비동기 관련 코드 작성

--- app/src/main/java/abled/semina/composeexample/MainViewModel.kt
+++ app/src/main/java/abled/semina/composeexample/MainViewModel.kt
@@ -1,14 +1,34 @@
 package abled.semina.composeexample
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import java.time.YearMonth
 
 class MainViewModel : ViewModel() {
 
+    // 상태를 관리할 StateFlow
+    private val _uiState = MutableStateFlow("초기 상태")
+    val uiState: StateFlow<String> = _uiState
+
     private val _currentMonth = MutableStateFlow(YearMonth.now())
     val currentMonth: StateFlow<YearMonth> = _currentMonth
+
+    init {
+        fetchData() // 데이터 비동기 요청
+    }
+
+    // 비동기 작업 (예: 네트워크 요청)
+    private fun fetchData() {
+        viewModelScope.launch {
+            // 예시: 비동기 작업 처리 후 상태 업데이트
+            delay(2000)
+            _uiState.value = "업데이트된 상태"
+        }
+    }
 
     // 이전 달로 이동
     fun goToPreviousMonth() {
